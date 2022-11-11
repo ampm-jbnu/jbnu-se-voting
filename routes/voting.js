543c27efb4568b8a9ab4e8309a13c37b68a04d8a
kiku99 <elsd0326@gmail.com>
2022-11-11 15:53:13 +0900

    서비스 열기

--- routes/voting.js
+++ routes/voting.js
@@ -4,8 +4,11 @@ const router = Router();
 import { home, voting_submit, voting_result } from '../constants/routes';
 import vc from '../controllers/votingController';
 
-// router.get(home, vc.checkVoting, vc.getVoting);
-router.get(home, vc.checkVoting);
+// 투표 진행시
+router.get(home, vc.checkVoting, vc.getVoting);
+
+// 투표 종료 시
+//router.get(home, vc.checkVoting);
 
 router.post(voting_submit, vc.checkVoting, vc.postVotingSubmit);
 
